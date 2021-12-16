7a719b9e63b5c2a6053d11a895a80bd522934246
ruskey97 <rusixin@inbox.ru>
2021-12-16 17:03:10 +0300

    5

---
+++ 5.py
@@ -0,0 +1,33 @@
+
+import sqlite3
+
+conn = sqlite3.connect('My_cats.db')
+cursor = conn.cursor()
+
+def get_type(idx):
+    res = list(cursor.execute(f"""
+                                   SELECT type from types where id={idx}
+                               """))
+    try:
+        return res[0][0]
+    except IndexError:
+        return
+
+def get_type_where(where):
+    for line in cursor.execute(f"""
+                                    SELECT type from types where {where}
+                                """):
+        print(*line)
+
+
+def get_all_types():
+    for line in cursor.execute(f"""
+                                    SELECT type from types
+                                """):
+        print(*line)
+
+
+print(get_type(56))
+get_type_where('id > 50')
+get_type_where('type like "%н"')
+get_all_types()
